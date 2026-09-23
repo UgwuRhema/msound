cf78964d4e6fe279e5b7236ddbf9586ae57e194e
UgwuRhema <hikarialiyev@gmail.com>
2026-09-23 17:19:43 +0000

    After some research found the wav fileHeader

---
+++ main.h
@@ -0,0 +1,22 @@
+#include <stdint.h>
+
+struct __attribute__((packed)) WavHeader
+{
+	uint8_t RIFF[4]; /* magic number identifying RIFF format, must be "RIFF" */
+	uint32_t ChunkSize; /* size of the entire file minus 8 bytes */
+	uint8_t WAVE[4]; /* format identifier, msut be "WAVE" */
+
+	uint8_t fmt[4]; /* sub chunk identifier, must be "fmt" */
+	uint32_t Subchunk1Size; /* size of the format sub-chunk */
+	uint16_t AudioFormat; /* available: 1, 6, 7, 257, 258, 259 */
+	uint16_t NumOfChan; /* number of audio channels, 1 = Mono, 2 = Stereo */
+	uint32_t SamplesPerSec; /* Freq in Hz */
+	uint32_t bytesPerSec;
+	uint16_t blockAlign; /* i give up...i ain't writing thse commecnts no more */
+	uint16_t bitsPerSample;
+
+	uint8_t Subchunk2ID[4];
+	uint32_t Subchunk2Size;
+};
+
+/* the complete header every .wav file must have */
