8d715ca1c335a5aec4afef040c649afffbbf1ead
UgwuRhema <hikarialiyev@gmail.com>
2026-09-23 15:28:42 +0000

    Trying to build a prompt checker

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 #include "main.h"
 
 int
-main()
+main(int argc, char *argv[])
 {
-
+	char *filename = (char *)malloc(sizeof(char) * 128);
+	/* if the user didn't pass a name */
+	if (argc < 2)
+	{
+		strcpy(filename, "music");
+	} else if (strcmp(argv[1], "-o") == 0 && argv[2] == NULL){
+		fprintf(stderr, "Enter a valid music filename\n");
+		return -1;
+	}
+	free(filename);
+	return 0;
 }
